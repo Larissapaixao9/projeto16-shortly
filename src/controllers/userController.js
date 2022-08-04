5636e5b8b1a07eecb7988fa41d9d205f1f19edef
Larissa Paixao <laryssapaixao9@gmail.com>
2022-08-04 09:52:30 -0300

    implementa conecção com banco de dados

---
+++ src/controllers/userController.js
@@ -0,0 +1,18 @@
+import express from "express";
+import connection from "../database";
+export async function logup(req,res){
+    const { name, email, password, confirmPassword } = req.body;
+try {
+    // if(!req.body){
+    //     res.send('erro na validação do cadastro').status(422)
+    // }
+
+    await connection.query(`INSERT INTO users (name, email, password, confirmPassword)
+    VALUES ($1,$2,$3,$4)`,[name, email, password, confirmPassword])
+
+    res.sendStatus(201)
+} catch (error) {
+    res.sendStatus(400)
+}
+    
+}
--- src/server.js
+++ src/server.js
@@ -1,16 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import userouters from './routers/userRouter.js'
 dotenv.config()
 
 const app=express();
 app.use(express.json())
 app.use(cors())
+app.use(userouters)
 
-app.get('/home',(req,res)=>{
+app.get('https://projetoshortly.herokuapp.com/home',(req,res)=>{
     res.send('funcionando')
 })
 
 
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>console.log('servidor funcionando'))
+
+//https://projetoshortly.herokuapp.com
